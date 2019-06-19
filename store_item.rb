a12a55e953125d0e7799b51bcd9d74996638e22b
shane <smp1993@gmail.com>
2019-06-18 20:45:07 -0400

    Changed the arguments to accept 1 argument instead of 3, using a hash

--- store_item.rb
+++ store_item.rb
@@ -23,10 +23,10 @@
 # p "The third bike is a #{bike_3["color"]} bike, worth $#{bike_3["price"]}, made in the year #{bike_3["year"]}."
 
 class Bike
-  def initialize(color, price, year)
-    @color = color
-    @price = price
-    @year = year
+  def initialize(bike_characteristics)
+    @color = bike_characteristics[:color]
+    @price = bike_characteristics[:price]
+    @year = bike_characteristics[:year]
   end
 
   attr_reader :color
@@ -34,6 +34,10 @@ class Bike
   attr_reader :price
   attr_writer :price
   attr_reader :year
+
+  def print_bike
+    p "The bike is a #{@color} bike, worth #{@price}, made in the year #{@year}"
+  end
 
 #   def color
 #     @color
@@ -53,22 +57,28 @@ class Bike
 
 end
 
-bike_1 = Bike.new("blue", 150, 2015)
+bike_1 = Bike.new(color: "blue", price: 150, year: 2015)
+bike_2 = Bike.new(color: "pink", price: 200, year: 2018)
+bike_3 = Bike.new(color: "yellow", price: 250, year: 2019)
 p bike_1
-
-bike_2 = Bike.new("pink", 200, 2018)
-p bike_2
-
-bike_2.color = "yellow"
-
-p bike_2.color
-
-p "The second bike is a #{bike_2.color} bike, worth #{bike_2.price}, from the year #{bike_2.year}."
-
-bike_3 = Bike.new("yellow", 250, 2019)
-
-p bike_3
-
+bike_1.print_bike
 p bike_1.color
-p bike_1.price
-p bike_1.year
+bike_2.print_bike
+bike_3.print_bike
+
+# bike_2 = Bike.new("pink", 200, 2018)
+# p bike_2
+
+# bike_2.color = "yellow"
+
+# p bike_2.color
+
+# p "The second bike is a #{bike_2.color} bike, worth #{bike_2.price}, from the year #{bike_2.year}."
+
+# bike_3 = Bike.new("yellow", 250, 2019)
+
+# p bike_3
+
+# p bike_1.color
+# p bike_1.price
+# p bike_1.year
